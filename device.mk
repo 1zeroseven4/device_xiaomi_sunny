374dcf79951e6c88e727ae7e0f0bc0533991e97f
1zeroseven4 <zerosevensign@gmail.com>
2025-05-18 10:12:28 +0800

    sunny: PixelOS -> AlphaDroid
    Signed-off-by: 1zeroseven4 <zerosevensign@gmail.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_ven
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Call the Leica Camera setup
+$(call inherit-product-if-exists, vendor/xiaomi/mojito-leicacamera/mojito-leicacamera-vendor.mk)
+
+# Signed keys
+-include vendor/alpha-priv/keys/keys.mk
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -373,7 +379,7 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # Use a profile based boot image for this device
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # Task Profiles
 PRODUCT_COPY_FILES += \
