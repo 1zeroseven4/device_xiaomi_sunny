67342c10512378a91d6a9e583145209eb7a8dd4d
1zeroseven4 <zerosevensign@gmail.com>
2026-03-16 21:46:50 +0800

    sunny: PixelOS -> VoltageOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/custom_sunny.mk
+    $(LOCAL_DIR)/voltage_sunny.mk
--- device.mk
+++ device.mk
@@ -16,6 +16,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_ven
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Inherit microG support
+WITH_MICROG ?= false
+ifeq ($(WITH_MICROG),true)
+    $(call inherit-product, vendor/microg/microg.mk)
+endif
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
