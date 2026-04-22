PixelOS-Devices/android_device_xiaomi_sunny

sunny: Opt out of speaker_layout_channel_mask field
Required for ABI compatibility with audio HAL compiled before 15 QPR2.

Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9426
Change-Id: I0bf7eafbeb19957326de04ecf7b903580315dc59
Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

device.mk
@@ -52,7 +52,9 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libvolumelistener \
+    libvolumelistener
+
+$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 
 # Audio XML
 PRODUCT_COPY_FILES += \

1zeroseven4/device_xiaomi_sunny

device.mk
@@ -52,7 +52,9 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libvolumelistener \
+    libvolumelistener
+
+$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 
 # Audio XML
 PRODUCT_COPY_FILES += \